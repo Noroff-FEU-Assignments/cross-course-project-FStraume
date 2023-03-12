ef341ea96c59bc4f74d4c07c235f9b0f94837e70
FStraume <98831659+FStraume@users.noreply.github.com>
2023-03-12 01:42:25 +0000

    Add files via upload

--- JS/product_spesific.js
+++ JS/product_spesific.js
@@ -2,36 +2,51 @@ const buttonAddToCart = document.querySelector(".productpage_ctaaddtocart");
 const buttonAddToWishlist = document.querySelector(".productpage_ctaaddtowishlist");
 const name = document.querySelector(".product_name");
 const price = document.querySelector(".product_price");
+const id = document.querySelector(".product_id")
 const itemcounter = document.querySelector(".itemcounter")
 const wishlistcounter = document.querySelector(".wishlistcounter")
+const checkoutSection = document.querySelector(".checkout_products")
+const productInfo = document.querySelector(".product_info")
+
+
+
+name.innerHTML = product.name;
+price.innerHTML = product.price;
+
+//----------------------------------------------------------------//
 
 const cart = []
-const wishlist = []
 
-console.log(buttonAddToCart)
+const cartlogger = []
+const wishlistlogger = []
 
-function addToCart() {
-        cart.push(name.innerHTML)
+function cartPusher() {
+        cartlogger.push(productInfo)
+        cart.push(productInfo)
         itemloggerCart()
         buttonAddToCart.innerHTML = "Added to cart"
+        addToCart()
 }
 
 function itemloggerCart() {
-    for (let i = 0; i < cart.length; i++) {
+    for (let i = 0; i < cartlogger.length; i++) {
         itemcounter.style.display = "flex";
-        itemcounter.innerHTML = cart.length; 
+        itemcounter.innerHTML = cartlogger.length; 
     }
 }
 
-function addToWishlist() {
-        wishlist.push(name.innerHTML)
+function wishlistPusher() {
+    wishlistlogger.push(name.innerHTML)
         itemloggerWishlist()
         buttonAddToWishlist.innerHTML = "Added to cart"
 }
 
 function itemloggerWishlist() {
-    for (let i = 0; i < wishlist.length; i++) {
+    for (let i = 0; i < wishlistlogger.length; i++) {
         wishlistcounter.style.display = "flex";
-        wishlistcounter.innerHTML = wishlist.length; 
+        wishlistcounter.innerHTML = wishlistlogger.length; 
     }
 } 
+
+//----------------------------------------------------------------//
+
---
+++ JS/productobject.js
@@ -0,0 +1,9 @@
+const product = {name:"Rain Jacket", 
+                price:"$299", 
+                id:50,
+                imgUrl: "/IMAGES/product_img.jpg"};
+
+
+
+
+
